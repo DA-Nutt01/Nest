6d7c87740c384fcdbec48e19f1e8b0aabef7d63c
DevonNutting <devon.nutting@hgs.hiddengeniusproject.org>
2023-05-09 01:07:50 -0700

    Unit Movement
    * Control Scheme changes based on the interactable currently selected
    * Selected Unit can move with left click
    * Created GameEnums scripts
    * Created Unit Script

--- Nest Prototype/Assets/Scripts/Controllers/PlayerController.cs
+++ Nest Prototype/Assets/Scripts/Controllers/PlayerController.cs
@@ -5,47 +5,109 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     #region Global Variables
+    // A mask to distinguish interactable objects from non interactable ones
     public LayerMask interactableLayer;
-    public Interactable selectedObject;
+    public LayerMask movementLayer;
+    // The current interactable the controller has selected 
+    public Interactable focus;
+    // The current control state for the controller to differentiate when the controls need to change in each state of gameplay
+    public ControlState controlState;
+    // Ref to the currentle selected unit if there is one
+    private Unit selectedUnit;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controlState = ControlState.defocused;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) // When Left click on the mouse is pressed...
+
+        switch(controlState)
         {
-            // A ref to the object hit by the raycast;
-            RaycastHit hit;  
-
-            // Create a ray from the camera to the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-
-            // If the ray hit an object on the interatable layer...
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity, interactableLayer))
-            {
-                // Store the Interactable script component off the object that was just hit in a var ref
-                Interactable interactable = hit.collider.GetComponent<Interactable>();
-
-                // If the the object we just hit has an Interactable component...
-                if(interactable != null)
+            case ControlState.defocused:
+                if(Input.GetMouseButtonDown(0)) // On Left Mouse Click
                 {
-                    // Set the interactable as the focus
-                    selectedObject = interactable;
-                    Debug.Log("Selected " + interactable.gameObject.name);
-                }
-            } else
-            {
-                selectedObject = null;
-                Debug.Log("Defocusing");
-            }
+                    // A ref to the object hit by the raycast;
+                    RaycastHit hit;  
 
-            
+                    // Create a ray from the camera to the mouse position
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+
+                    // If the ray hit an object on the interatable layer...
+                    if(Physics.Raycast(ray, out hit, Mathf.Infinity, interactableLayer))
+                    {
+                        // Store the Interactable script component off the object that was just hit in a var ref
+                        Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+                        // If the the object we just hit has an Interactable component...
+                        if(interactable != null)
+                        {
+                            // Set the interactable as the focus
+                            SetFocus(interactable);
+                            controlState = ControlState.unitSelected;
+                        }
+                    }
+                }
+                break;
+                
+            case ControlState.unitSelected:
+                if(Input.GetMouseButtonDown(0)) //On Left Mouse Click...
+                    {
+                    // A ref to the object hit by the raycast;
+                    RaycastHit hit;  
+
+                    // Create a ray from the camera to the mouse position
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    // If the ray hit anything on the movement layer, such as the ground...
+                    if(Physics.Raycast(ray, out hit, Mathf.Infinity, movementLayer))
+                    {
+                        //Move the Unit to that location
+                        selectedUnit.Move(hit.point);
+                    }
+                }
+                break;
+        }
+
+        if(Input.GetMouseButtonDown(1)) // On Right Mouse Click
+        {
+            Defocus();
+        }
+    }
+
+    private void SetFocus(Interactable interactable)
+    {
+        // Checks if the interactable is a Unit or not
+        switch (interactable.GetComponent<Unit>())
+        {
+            case null: // Interactable is not a unit
+                Debug.Log("Interactable is not a unit, Selected" + interactable.gameObject.name);
+                // Set current focus to this interactable
+                focus = interactable;
+                break;
+            default: // Interactable is a unit
+                Debug.Log("Interactable is a unit, Selected " + interactable.gameObject.name);
+                // Set current focus to this interactable
+                focus = interactable;
+                // Grab ref to Unit component to access this unit's functions
+                selectedUnit = interactable.GetComponent<Unit>();
+                break;
+        }
+    }
+
+    private void Defocus()
+    {
+        // We only need to defocus if an interactable is currently being focused on
+        if(focus != null) 
+        {
+            focus = null;
+            controlState = ControlState.defocused;
+            selectedUnit = null;
+            Debug.Log("Defocusing");
         }
     }
 }
---
+++ Nest Prototype/Assets/Scripts/GameEnums.cs
@@ -0,0 +1,5 @@
+public enum ControlState 
+{
+    defocused,    // No interactable is focused on 
+    unitSelected, // A unit is currently focused on
+}
